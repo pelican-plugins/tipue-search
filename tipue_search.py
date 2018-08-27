731de24426b5138a367c96d5933a284405496f78
nkr <42086312+nkralpy@users.noreply.github.com>
2018-08-27 14:18:24 +0200

    update to change tipuesearch_content output as js var instead of json

--- tipue_search.py
+++ tipue_search.py
@@ -83,7 +83,7 @@ class Tipue_Search_JSON_Generator(object):
 
 
     def generate_output(self, writer):
-        path = os.path.join(self.output_path, 'tipuesearch_content.json')
+        path = os.path.join(self.output_path, 'tipuesearch_content.js')
 
         pages = self.context['pages'] + self.context['articles']
 
@@ -97,8 +97,10 @@ class Tipue_Search_JSON_Generator(object):
             self.create_json_node(page)
         root_node = {'pages': self.json_nodes}
 
+        root_node_js = 'var tipuesearch = ' + json.dumps(root_node, separators=(',', ':'), ensure_ascii=False) + ';'
+
         with open(path, 'w', encoding='utf-8') as fd:
-            json.dump(root_node, fd, separators=(',', ':'), ensure_ascii=False)
+            fd.write(root_node_js)
 
 
 def get_generators(generators):
